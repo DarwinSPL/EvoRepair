941f581afb1ee0d5041fce109cac85abe8b3da13
Gil Engel <g.engel@tu-bs.de>
2017-10-16 20:50:05 +0200

    Clean projects

--- plugins/de.evorepair.xtext.operation/src/de/evorepair/xtext/EvoOperationQualifiedNameProvider.java
+++
@@ -1,7 +0,0 @@
-package de.evorepair.xtext;
-
-import org.eclipse.xtext.naming.DefaultDeclarativeQualifiedNameProvider;
-
-public class EvoOperationQualifiedNameProvider extends DefaultDeclarativeQualifiedNameProvider{
-
-}
--- plugins/de.evorepair.xtext.operation/src/de/evorepair/xtext/EvoOperationResourceDescriptionStrategy.java
+++
@@ -1,7 +0,0 @@
-package de.evorepair.xtext;
-
-import org.eclipse.xtext.resource.impl.DefaultResourceDescriptionStrategy;
-
-public class EvoOperationResourceDescriptionStrategy extends DefaultResourceDescriptionStrategy {
-
-}
